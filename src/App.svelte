6191346b66c0b70e0d909a76c55b9d307a2162c0
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-14 15:58:01 +0500

    Allow Sass variables to be used globally and locally

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 </script>
 
-<div />
+<div>
+  <h1>New app</h1>
+</div>
 
-<style>
+<!-- Global styles -->
+<svelte:head>
+  <style lang="scss">
+    body {
+      background-color: red;
+
+      h1 {
+        color: white;
+      }
+    }
+  </style>
+</svelte:head>
+
+<!-- App styles -->
+<style lang="scss">
+  div {
+    font-family: monospace;
+  }
 </style>
