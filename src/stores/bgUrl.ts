c9e6c4b0507002c73752c9a94055e5b6dcaa917a
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-14 17:15:08 +0500

    Create setBackground in bgUrl

--- src/stores/bgUrl.ts
+++ src/stores/bgUrl.ts
@@ -1,9 +1,23 @@
+import axios from "axios";
 import { writable } from "svelte/store";
+
+const UNSPLASH_API_URL = "https://api.unsplash.com/";
 
 export const bgUrl = (() => {
   const { subscribe, set } = writable<string>(null);
 
-  const setBackground = (query: string) => {};
+  const setBackground = async (query: string) => {
+    const res = await axios.get(UNSPLASH_API_URL + "photos/random", {
+      params: {
+        query,
+        client_id: import.meta.env.VITE_UNSPLASH_ACCESS_KEY,
+      },
+    });
+
+    const url: string = res.data.urls.regular;
+
+    set(url);
+  };
 
   return { subscribe, setBackground };
 })();
