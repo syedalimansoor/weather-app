1d329ee9a3e145ef9d003284658192267892b750
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-18 16:46:00 +0500

    Create Header

---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Logo from "./Logo.svelte";
+  import UnitToggle from "./UnitToggle.svelte";
+</script>
+
+<header class="header">
+  <Logo />
+  <UnitToggle />
+</header>
+
+<style lang="scss">
+  .header {
+    padding: 1.5rem;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/lib/Header/Logo.svelte
@@ -0,0 +1,9 @@
+<h1 class="logo">Weather</h1>
+
+<style lang="scss">
+  .logo {
+    font-size: $fs-medium;
+    font-style: italic;
+    pointer-events: none;
+  }
+</style>
