85bbe8b650c28792e090d62abba58e31eb07768e
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-18 22:25:24 +0500

    Implement value if editing, otherwise state

---
+++ src/lib/SearchBar/SearchBar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import SearchField from "./SearchField.svelte";
+  import CountryFlag from "$lib/CountryFlag.svelte";
+</script>
+
+<label for="search">Search for a city</label>
+<SearchField />
+<CountryFlag />
---
+++ src/lib/SearchBar/SearchField.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { location } from "$stores/index";
+
+  let editing: boolean = false;
+  let value: string = "";
+
+  const handleFocus = () => {
+    editing = true;
+    value = "";
+  };
+
+  const handleBlur = () => {
+    editing = false;
+  };
+
+  $: {
+    if (!editing) value = $location.name;
+  }
+</script>
+
+<input
+  type="search"
+  id="search"
+  on:focus={handleFocus}
+  on:blur={handleBlur}
+  bind:value
+/>
