5d3cb875fb870c7ad61d69d5dcf2a8dd1ee15ae8
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-18 23:35:56 +0500

    Style SearchField

--- src/lib/SearchBar/SearchBar.svelte
+++ src/lib/SearchBar/SearchBar.svelte
@@ -3,6 +3,19 @@
   import CountryFlag from "$lib/CountryFlag.svelte";
 </script>
 
-<label for="search">Search for a city</label>
-<SearchField />
-<CountryFlag />
+<label class="label" for="search">Search for a city</label>
+<div class="field">
+  <SearchField />
+  <CountryFlag />
+</div>
+
+<style lang="scss">
+  .label {
+    display: block;
+    margin-bottom: 0.8rem;
+  }
+
+  .field {
+    display: flex;
+  }
+</style>
--- src/lib/SearchBar/SearchField.svelte
+++ src/lib/SearchBar/SearchField.svelte
@@ -14,14 +14,42 @@
   };
 
   $: {
-    if (!editing) value = $location.name;
+    if (!editing && $location.name)
+      value = `${$location.name}, ${$location.country}`;
   }
 </script>
 
-<input
-  type="search"
-  id="search"
-  on:focus={handleFocus}
-  on:blur={handleBlur}
-  bind:value
-/>
+<div class="wrapper">
+  <input
+    class="field"
+    id="search"
+    on:focus={handleFocus}
+    on:blur={handleBlur}
+    bind:value
+  />
+</div>
+
+<style lang="scss">
+  .wrapper {
+    flex-grow: 1;
+    position: relative;
+
+    &::before {
+      content: url("$assets/icon-search.svg");
+      position: absolute;
+      left: 0.8rem;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+
+  .field {
+    border: none;
+    display: inline-block;
+    font-size: $fs-small;
+    background-color: $white;
+    padding: 0.6rem 0.8rem;
+    text-align: right;
+    width: 100%;
+  }
+</style>
