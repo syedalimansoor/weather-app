9540b4f07c128b60473b64ff197890ce708cee77
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-21 00:32:19 +0500

    Implement DataList with minimal styling

--- src/lib/SearchBar/DataList.svelte
+++ src/lib/SearchBar/DataList.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
   import type { City } from "$src/stores/location";
 
+  import DataListItem from "./DataListItem.svelte";
+
   export let cities: City[];
 </script>
 
-<div>
-  {JSON.stringify(cities)}
+<div class="datalist-wrapper">
+  {#if cities}
+    <ul class="datalist">
+      {#each cities as city}
+        <DataListItem {city} />
+      {/each}
+    </ul>
+  {:else}
+    loading
+  {/if}
 </div>
+
+<style lang="scss">
+  .datalist-wrapper {
+    display: grid;
+    place-items: center;
+    color: $black;
+    min-height: 12em;
+  }
+  .datalist {
+    padding: 0;
+    list-style-type: none;
+    height: 100%;
+    width: 100%;
+  }
+</style>
