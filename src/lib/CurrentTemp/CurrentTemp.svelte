8be68267778981704bdc80749d461893d7ff5338
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-24 22:57:13 +0500

    Add basic elements

--- src/lib/CurrentTemp/CurrentTemp.svelte
+++ src/lib/CurrentTemp/CurrentTemp.svelte
@@ -2,6 +2,7 @@
   import axios from "axios";
   import { location, unit } from "$src/stores";
   import type { CurrentTemp } from "./curr_temp";
+  import { convertTemp } from "$src/utils/convertTemp";
 
   let currentTemp: CurrentTemp = {
     temp: null,
@@ -10,25 +11,42 @@
     description: null,
   };
 
-  location.subscribe(async (location) => {
-    if (location.name) {
-      const { data } = await axios(
-        "https://api.openweathermap.org/data/2.5/weather",
-        {
-          params: {
-            lat: location.lat,
-            lon: location.lon,
-            appid: import.meta.env.VITE_WEATHER_API_KEY,
-          },
-        }
-      );
-
-      currentTemp.temp = data.main.temp;
-      currentTemp.feels_like = data.main.feels_like;
-      currentTemp.humidity = data.main.humidity;
-      currentTemp.description = data.weather[0].description;
+  // Fetch weather data and store in currentTemp
+  $: {
+    if ($location.name) {
+      axios("https://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          lat: $location.lat,
+          lon: $location.lon,
+          appid: import.meta.env.VITE_WEATHER_API_KEY,
+        },
+      }).then(({ data }) => {
+        currentTemp.temp = data.main.temp;
+        currentTemp.feels_like = data.main.feels_like;
+        currentTemp.humidity = data.main.humidity;
+        currentTemp.description = data.weather[0].description;
+      });
     }
-  });
+  }
+
+  // Convert temperature values and round down
+  $: temp = Math.round(convertTemp(currentTemp.temp, $unit));
+  $: feels_like = Math.round(convertTemp(currentTemp.feels_like, $unit));
 </script>
 
+<div class="wrapper">
+  <h2 class="current-temp">{temp}&deg;{$unit}</h2>
+  <p class="feels-like">Feels like {feels_like}&deg;{$unit}</p>
+</div>
 
+<style lang="scss">
+  .current-temp {
+    font-size: $fs-xlarge;
+    font-weight: $fw-reg;
+    line-height: 1.2;
+  }
+
+  .feels-like {
+    font-size: $fs-medium;
+  }
+</style>
