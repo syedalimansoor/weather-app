bbdc0abf2f99ad003683276274036a4dc49eeab8
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-24 22:56:24 +0500

    Create convert temp function

---
+++ src/utils/convertTemp.ts
@@ -0,0 +1,12 @@
+import { get } from "svelte/store";
+
+export const convertTemp = (temp: number, toUnit: "C" | "F") => {
+  switch (toUnit) {
+    case "C":
+      return temp - 273.15;
+    case "F":
+      return (temp - 273.15) * (9 / 5) + 32;
+    default:
+      throw new Error("Invalid value stored in temp");
+  }
+};
