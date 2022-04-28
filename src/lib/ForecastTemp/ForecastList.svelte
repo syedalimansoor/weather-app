5a589c4de59fc12bb7b2f762998cf0289160c666
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-04-29 01:20:04 +0500

    Fetch forecast data on location changes

--- src/lib/ForecastTemp/ForecastList.svelte
+++ src/lib/ForecastTemp/ForecastList.svelte
@@ -1,3 +1,33 @@
 <script lang="ts">
-  export let mode: "hourly"|"daily";
+  import { location } from "$src/stores";
+  import axios from "axios";
+  import ForecastItem from "./ForecastItem.svelte";
+  export let mode: "hourly" | "daily";
+
+  const FORECAST_API_URL = "https://api.openweathermap.org/data/2.5/forecast";
+
+  let tempList: Array<{
+    dt_txt: string;
+    main: {
+      temp: number;
+    };
+    weather: Array<{
+      description: string;
+      id: number;
+    }>;
+  }> = [];
+
+  location.subscribe((location) => {
+    if (location.name) {
+      axios(FORECAST_API_URL, {
+        params: {
+          lat: location.lat,
+          lon: location.lon,
+          appid: import.meta.env.VITE_WEATHER_API_KEY,
+        },
+      }).then(({ data }) => {
+        tempList = data.list;
+      });
+    }
+  });
 </script>
