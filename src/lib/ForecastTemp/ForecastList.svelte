6666ec320e0171f90368ae14a7006b1cdbfdf1fd
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-09 18:30:54 +0500

    Implement forecast data skeleton

--- src/lib/ForecastTemp/ForecastList.svelte
+++ src/lib/ForecastTemp/ForecastList.svelte
@@ -32,12 +32,21 @@
   });
 </script>
 
-<div class="forecast-list">
-  {#each tempList as temp, index}
-    {#if (mode === "hourly" && index < 10) || (mode === "daily" && index % 8 === 0)}
-      <ForecastItem {temp} {mode} />
-    {/if}
-  {/each}
+<div class="forecast-list" class:skeleton={!tempList.length}>
+  {#if tempList.length}
+    {#each tempList as temp, index}
+      {#if (mode === "hourly" && index < 10) || (mode === "daily" && index % 8 === 0)}
+        <ForecastItem {temp} {mode} />
+      {/if}
+    {/each}
+  {:else}
+    {#each Array(10) as _}
+      <div class="skeleton-item" role="presentation">
+        <div class="skeleton-icon" />
+        <div class="skeleton-label" />
+      </div>
+    {/each}
+  {/if}
 </div>
 
 <style lang="scss">
@@ -47,5 +56,48 @@
     padding-block: 1em;
     border-block: 1px solid $white;
     scroll-snap-type: x mandatory;
+  }
+
+  .skeleton {
+    justify-content: space-evenly;
+    gap: 2em;
+
+    &-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 2em;
+      padding-block: 1em;
+    }
+
+    &-icon {
+      width: 100px;
+      aspect-ratio: 1;
+    }
+
+    &-label {
+      width: 120px;
+      height: 30px;
+    }
+
+    &-icon,
+    &-label {
+      background: $transparent-gray;
+      background: linear-gradient(
+        110deg,
+        $transparent-gray 8%,
+        $white 18%,
+        $transparent-gray 33%
+      );
+      background-size: 200% 100%;
+      transition: 200ms ease;
+      animation: shine 1.5s infinite linear;
+    }
+  }
+
+  @keyframes shine {
+    to {
+      background-position-x: -200%;
+    }
   }
 </style>
